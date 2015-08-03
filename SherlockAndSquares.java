e14f0c959df9c2541e04e2692402af1e65684f79
d00mk1n <Lev@MacBook-Lev.local>
2015-08-03 14:38:59 +0300

    изменил форматирование и счетчик в основном цикле

--- SherlockAndSquares.java
+++ SherlockAndSquares.java
@@ -4,79 +4,67 @@ import java.util.Scanner;
 
 public class SherlockAndSquares {
 
-	public static void add (int[] tests, int t) 
-	{
-		for (int i = 0; i < tests.length; i++) 
-		{
-			if (tests[i] == 0) 			
-			{
+	public static void add(int[] tests, int t) {
+		for (int i = 0; i < tests.length; i++) {
+			if (tests[i] == 0) {
 				tests[i] = t;
 				break;
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		if (!scanner.hasNextInt())
-		{
+		if (!scanner.hasNextInt()) {
 			System.out.println("incorrect number of tries entered");
 			System.exit(0);
 		}
 		int numberOfTries = scanner.nextInt();
-		
-		if (numberOfTries < 0 || numberOfTries > 100)
-		{
+
+		if (numberOfTries < 0 || numberOfTries > 100) {
 			System.out.println("incorrect number of tries entered");
 			System.exit(0);
-			}
-	
-		int tests[] = new int[numberOfTries*2];
-		
-		for (int i = 0; i < tests.length; i++)
-		{
-			if (!scanner.hasNextInt())
-			{
+		}
+
+		int tests[] = new int[numberOfTries * 2];
+
+		for (int i = 0; i < tests.length; i++) {
+			if (!scanner.hasNextInt()) {
 				System.out.println("incorrect number format");
 				System.exit(0);
 			}
 			int t = scanner.nextInt();
-			if (t < 0 || t > 1000000000) 
-			{
+			if (t < 0 || t > 1000000000) {
 				System.out.println("incorrect number entered");
-				System.exit(0);	
+				System.exit(0);
 			}
-			add (tests, t);
+			add(tests, t);
 		}
-		
-		for (int i = 0; i < tests.length; i++, i++)
-		{
+
+		for (int i = 0; i < tests.length; i += 2) {
 			int n0 = tests[i];
-			
-			int n1 = tests[i+1];
-			
-			if (n0 > n1)
-			{
+
+			int n1 = tests[i + 1];
+
+			if (n0 > n1) {
 				System.out.println("incorrect period of numbers entered");
 				System.exit(0);
 			}
-			
+
 			int count = 0;
-			
-			for (int j = n0; j <= n1; j++) 
-			{
+
+			for (int j = n0; j <= n1; j++) {
 				double s = Math.sqrt(j);
-				if (s % 1 == 0) 
-				{
+				if (s % 1 == 0) {
 					count++;
 				}
 			}
-			
+
 			System.out.println(count);
-			
+
 		}
-		
+
 	}
 
 }
