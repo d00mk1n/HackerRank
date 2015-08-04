25b14ca80fd6cd476cab76529f50b3ccd7503494
d00mk1n <Lev@MacBook-Lev.local>
2015-08-04 18:11:11 +0300

    FunnyString: Добавил алгоритм

--- FunnyString.java
+++ FunnyString.java
@@ -19,6 +19,26 @@ public class FunnyString {
 		System.out.println(string);
 		System.out.println(stringR);
 		
+		int count = 0;
+		
+		for (int i = 1; i < string.length(); i++) {
+			int c1 = string.charAt(i) - string.charAt(i-1);
+			int c2 = stringR.charAt(i) - stringR.charAt(i-1);
+	if ((c1 != c2) && (c1 != c2*(-1))) {
+		count++;
+		
+	}
+			
+//			System.out.println(c1);
+//			System.out.println(c2);
+
+		}
+		if (count == 0) {
+			System.out.println("FUNNY");
+		} else {
+			System.out.println("NOT FUNNY");
+			
+		}
 	}
 
 }
