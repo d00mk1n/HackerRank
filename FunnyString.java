1f2e96b7eb796d941a0c57eea5a47c96df8879c8
d00mk1n <Lev@MacBook-Lev.local>
2015-08-05 13:50:54 +0300

    FunnyString: изменил ввод, чтобы соответствовать условиям задачи (много строк)

--- FunnyString.java
+++ FunnyString.java
@@ -1,44 +1,44 @@
 package hackerRank;
+
 import java.util.Scanner;
 
 public class FunnyString {
-		
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in); 
-		
-		String string = scanner.nextLine();
-		
-		char chars[] = new char[string.length()];
-		
-		for (int i=string.length()-1; i >= 0; i--) 
-		{
-			chars[string.length()-1-i] = string.charAt(i);
-		}
-		String stringR = new String(chars);
-		
-		System.out.println(string);
-		System.out.println(stringR);
-		
-		int count = 0;
-		
-		for (int i = 1; i < string.length(); i++) {
-			int c1 = string.charAt(i) - string.charAt(i-1);
-			int c2 = stringR.charAt(i) - stringR.charAt(i-1);
-	if ((c1 != c2) && (c1 != c2*(-1))) {
-		count++;
-		
-	}
-			
-//			System.out.println(c1);
-//			System.out.println(c2);
+		Scanner scanner = new Scanner(System.in);
+		int numberOfTests = Integer.parseInt(scanner.nextLine());
 
+		String strings[] = new String[numberOfTests];
+		for (int i = 0; i < strings.length; i++) {
+			strings[i] = scanner.nextLine();
 		}
-		if (count == 0) {
-			System.out.println("FUNNY");
-		} else {
-			System.out.println("NOT FUNNY");
+		for (int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
 			
 		}
-	}
+		for (int i = 0; i < strings.length; i++) {
+			char chars[] = new char[strings[i].length()];
+			int l = 0;
+			for (l = strings[i].length() - 1; l >= 0; l--) {
+				chars[strings[i].length() - 1 - l] = strings[i].charAt(l);
+			}
+			String stringR = new String(chars);
 
+			int count = 0;
+
+			for (int j = 1; j < strings[i].length(); j++) {
+				int c1 = strings[i].charAt(j) - strings[i].charAt(j - 1);
+				int c2 = stringR.charAt(j) - stringR.charAt(j - 1);
+				if ((c1 != c2) && (c1 != c2 * (-1))) {
+					count++;
+				}
+			}
+			if (count == 0) {
+				System.out.println("FUNNY");
+			} else {
+				System.out.println("NOT FUNNY");
+
+			}
+		}
+	}
 }
