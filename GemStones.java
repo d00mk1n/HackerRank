4fc1c3025cf69146ae2693b31c5dff7adf3fd777
d00mk1n <Lev@MacBook-Lev.local>
2015-08-07 16:42:24 +0300

    GemStones: добавил метод для выделения уникальных знаков из строки

--- GemStones.java
+++ GemStones.java
@@ -1,7 +1,29 @@
 package hackerRank;
+
 import java.util.Scanner;
 
 public class GemStones {
+
+	public static char[] extChars(String s) {
+		char[] chars = new char[s.length()];
+		int countChar = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int count = 0;
+			for (int j = 0; j < chars.length; j++) {
+				if (s.charAt(i) == chars[j]) {
+					count++;
+				}
+			}  if (count == 0) {
+				chars[countChar] = s.charAt(i);
+				countChar++;
+			}
+		}
+		return chars;
+	}
+	
+	
+		
+		
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -11,7 +33,12 @@ public class GemStones {
 		String strings[] = new String[numberOfTests];
 		for (int i = 0; i < strings.length; i++) {
 			strings[i] = scanner.nextLine();
-			
+		}
+		
+		for (int i = 0; i < strings.length; i++) {
+			char[] chars = new char[strings[i].length()];
+			chars = extChars(strings[i]);
+			System.out.println(chars);
 		}
 	}
 }
