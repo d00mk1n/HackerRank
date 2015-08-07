946a4e7eb014aa50e3474872e6f91d38a109c900
d00mk1n <Lev@MacBook-Lev.local>
2015-08-07 12:27:37 +0300

    FansAndBuns: изменил ввод

--- FansAndBuns.java
+++ FansAndBuns.java
@@ -1,20 +1,33 @@
 package hackerRank;
+
 import java.util.Scanner;
 
 public class FansAndBuns {
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		int numberOfFans = scanner.nextInt();
-		
-		int[] timeOfOrder = new int[numberOfFans];
-		int[] duration = new int[numberOfFans];
-	
-		for (int i = 0; i < numberOfFans; i++) {
-			timeOfOrder[i] = scanner.nextInt();
-			duration[i] = scanner.nextInt();
-		}	
+
+	static class Fan {
+
+		int time;
+		int dur;
+		int total = time + dur;
+		int num;
+
+		public Fan(int time, int dur, int num) {
+			this.time = time;
+			this.dur = dur;
+			this.num = num;
+		}
+
+		public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+
+			int numberOfFans = scanner.nextInt();
+
+			Fan[] fans = new Fan[numberOfFans];
+
+			for (int i = 0; i < numberOfFans; i++) {
+				fans[i] = new Fan(scanner.nextInt(), scanner.nextInt(), i);
+			}
+		}
 	}
 }
