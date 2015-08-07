4317a578454fc4e6bfb7f34aa8cbd0b73532866b
d00mk1n <Lev@MacBook-Lev.local>
2015-08-07 14:38:23 +0300

    MarkAndToys: исправил баг, при котором получалась ошибка, если денег хватало на все игрушки сразу

--- MarkAndToys.java
+++ MarkAndToys.java
@@ -1,4 +1,5 @@
 package hackerRank;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,24 +8,26 @@ public class MarkAndToys {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int numberOfToys = scanner.nextInt();
 		int money = scanner.nextInt();
-		
+
 		int[] toys = new int[numberOfToys];
 
 		for (int i = 0; i < numberOfToys; i++) {
 			toys[i] = scanner.nextInt();
 		}
-		
+
 		Arrays.sort(toys);
 
 		int sum = 0;
 		int i = 0;
-		while (sum <= money) {
+		while (sum <= money && i < toys.length) {
 			sum = sum + toys[i];
-			i++;
+			if (sum <= money) {
+				i++;
+			}
 		}
-		System.out.println(i - 1);
+		System.out.println(i);
 	}
 }
