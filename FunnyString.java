c685c5611ad582b20ae1cd304011ecab736678ed
d00mk1n <Lev@MacBook-Lev.local>
2015-08-09 01:07:05 +0300

    FunnyString: вывел решение в отдельный метод

--- FunnyString.java
+++ FunnyString.java
@@ -3,28 +3,43 @@ package hackerRank;
 import java.util.Scanner;
 
 public class FunnyString {
-
+	
+	private static String originalString;
+	
+	public FunnyString(String originalString) {
+		this.originalString = originalString;
+	}
+	
+public static boolean isFunny (String originalString) {
+	String stringR = new StringBuilder(originalString).reverse().toString();
+	for (int j = 1; j < originalString.length(); j++) {
+		int c1 = Math.abs(originalString.charAt(j) - originalString.charAt(j - 1));
+		int c2 = Math.abs(stringR.charAt(j) - stringR.charAt(j - 1));
+		if (c1 - c2 != 0) {
+			return false;
+		}
+	}
+	return true;
+}
 	public static void main(String[] args) {
 		String[] strings = readInput();
-
 		for (int i = 0; i < strings.length; i++) {
-			String stringR = new StringBuilder(strings[i]).reverse().toString();
-			int count = 0;
-
-			for (int j = 1; j < strings[i].length(); j++) {
-				int c1 = Math.abs(strings[i].charAt(j) - strings[i].charAt(j - 1));
-				int c2 = Math.abs(stringR.charAt(j) - stringR.charAt(j - 1));
-				if (c1 - c2 != 0) {
-					count++;
-				}
-			}
-			if (count == 0) {
-				System.out.println("FUNNY");
+			if (isFunny(strings[i])) {
+				System.out.println("Funny");
 			} else {
-				System.out.println("NOT FUNNY");
-
+				System.out.println("Not Funny");
 			}
 		}
+//		for (int i = 0; i < strings.length; i++) {
+//			int count = 0;
+//
+//			if (count == 0) {
+//				System.out.println("FUNNY");
+//			} else {
+//				System.out.println("NOT FUNNY");
+//
+//			}
+//		}
 	}
 
 	private static String[] readInput() {
