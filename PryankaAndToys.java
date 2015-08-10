e10b13afbbe19cd3b52ddca1af7ffd1915d533e5
d00mk1n <Lev@MacBook-Lev.local>
2015-08-10 10:03:44 +0300

    PryankaAndToys: исправил решение, теперь оно верное, закрыл сканер

--- PryankaAndToys.java
+++ PryankaAndToys.java
@@ -7,33 +7,33 @@ public class PryankaAndToys {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-
-		int numberOfToys = scanner.nextInt();
-
-		int[] toys = new int[numberOfToys];
-
-		for (int i = 0; i < toys.length; i++) {
-			toys[i] = scanner.nextInt();
-		}
+		int[] toys = readInput();
 		Arrays.sort(toys);
-		int count = 0;
 		int money = 0;
-		boolean smthBougth;
-		for (int i = toys[0]; i <= toys[toys.length - 1]; i += 4) {
-			smthBougth = false;
-			for (int j = 0; j < toys.length; j++) {
-				if (toys[j] >= i && toys[j] <= i + 4) {
-					count++;
-					smthBougth = true;
-				}
+		int p = 0;
+		while (p < toys.length) {
+			money++;
+			int firstToy = toys[p];
+			while (p < toys.length && toys[p] <= firstToy + 4) {
+				p++;
 			}
-			if (smthBougth) {
-				money++;
+		}
+		System.out.println(money);
+	}
+
+	private static int[] readInput() {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			int numberOfToys = scanner.nextInt();
+
+			int[] toys = new int[numberOfToys];
+
+			for (int i = 0; i < toys.length; i++) {
+				toys[i] = scanner.nextInt();
 			}
-			if (count == toys.length) {
-				System.out.println(money);
-			}
+			return toys;
+		} finally {
+			scanner.close();
 		}
 	}
 }
