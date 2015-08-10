e7793b4d550be918359d6f86ee50a0c582571c80
d00mk1n <Lev@MacBook-Lev.local>
2015-08-10 07:06:00 +0300

    TwoStrings: изменил алгоритм решения

--- TwoStrings.java
+++ TwoStrings.java
@@ -4,32 +4,44 @@ import java.util.Scanner;
 
 public class TwoStrings {
 
-	public static void main(String[] args) {
+	public static boolean[] extrChars (String s) {
+		boolean[] present = new boolean[26];
+		for (int i = 0; i < s.length(); i++ ) {
+			present [s.charAt(i) - 'a'] = true;
+		} return present;
+	}
+
+	private static String[] readInput() {
 
 		Scanner scanner = new Scanner(System.in);
 
-		int numberOfTests = Integer.parseInt(scanner.nextLine());
+		try {
+			int numberOfTests = Integer.parseInt(scanner.nextLine());
 
-		String strings[] = new String[numberOfTests * 2];
+			String strings[] = new String[numberOfTests * 2];
 
-		for (int i = 0; i < strings.length; i++) {
-			strings[i] = scanner.nextLine();
+			for (int i = 0; i < strings.length; i++) {
+				strings[i] = scanner.nextLine();
+			}
+			return strings;
+		} finally {
+			scanner.close();
 		}
+	}
+	public static void main(String[] args) {
 
-		for (int i = 0; i < strings.length; i += 2) {
-			int count = 0;
-			for (int j = 0; j < strings[i].length(); j++) {
-				for (int l = 0; l < strings[i + 1].length(); l++) {
-					if (strings[i].charAt(j) == strings[i + 1].charAt(l)) {
-						count++;
-					}
+		String[] strings = readInput();
+
+		outer: for (int i = 0; i < strings.length; i+=2) {
+			boolean[] t1 = extrChars(strings[i]);
+			boolean[] t2 = extrChars(strings[i+1]);
+			for (int j = 0; j < 26; j++) {
+				if (t1[j] && t2[j]) {
+					System.out.println("YES");
+					continue outer;
 				}
 			}
-			if (count == 0) {
-				System.out.println("NO");
-			} else {
-			System.out.println("YES");
-			}
+			System.out.println("NO");
 		}
 	}
 }
